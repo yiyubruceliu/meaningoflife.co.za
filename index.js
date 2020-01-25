b29c6d45d05fb4e3061d6111ab1f6d94d5e2efc6
bruce liu <yiyubruceliu@gmail.com>
2020-01-25 03:18:41 +0200

    changes

---
+++ index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+window.$ = window.jQuery = $;
+import jquerymigrate from "jquery-migrate";
+import bootstrap from "bootstrap";
+const cleanBlog = require("./js/clean-blog.min.js");
